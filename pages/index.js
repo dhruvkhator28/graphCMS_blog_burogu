4c2cec83b4d7eedfa42163ceb682c29983342c27
Dhruv Khator <86736009+dhruvkhator28@users.noreply.github.com>
2021-12-19 20:18:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { FeaturedPosts } from '../sections';
 
 
 export default function Home({ posts }) {
-  console.log(posts);
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
